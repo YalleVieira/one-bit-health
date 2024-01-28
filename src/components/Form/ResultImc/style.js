0dcde54a4620d272bb9df32c04466a13b9b1cdcd
Yalle <yallevieira99@gmail.com>
2024-01-28 17:20:23 -0300

    adicionando estilização

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  resultImc: {
+    flex: 1,
+    marginTop: 15,
+    paddingTop: 15,
+    borderRadius: 50,
+    alignItems: "center",
+    width: "100%",
+  },
+  numberImc: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "#B22222",
+    fontWeight: "bold",
+  },
+  information: {
+    fontSize: 18,
+    color: "#B22222",
+    fontWeight: "bold",
+  },
+});
+export default styles;
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import ResultImc from "./ResultImc/";
+import styles from "./style";
 
 export default function Title() {
   const [height, setHeight] = useState(null),
@@ -17,35 +18,42 @@ export default function Title() {
   function validationImc() {
     if (weight && height) {
       imcCalculator();
-      setHeight(0);
-      setWeight(0);
+      setHeight(null);
+      setWeight(null);
       setMessageImc("Seu IMC é:");
       setTextButton("Calcular novamente");
     } else {
-      setImc(0);
+      setImc(null);
       setTextButton("Calcular IMC");
       setMessageImc("Preencha o peso e altura");
     }
   }
 
   return (
-    <View>
-      <View>
-        <Text>Altura</Text>
+    <View style={styles.formContent}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
         <TextInput
+          style={styles.input}
           value={height}
           onChangeText={(text) => setHeight(text)}
           placeholder="Ex. 1.75"
           keyboardType="numeric"
         />
-        <Text>Peso</Text>
+        <Text style={styles.formLabel}>Peso</Text>
         <TextInput
+          style={styles.input}
           value={weight}
           onChangeText={(text) => setWeight(text)}
           placeholder="Ex. 75.365"
           keyboardType="numeric"
         />
-        <Button onPress={() => validationImc()} title={textButton} />
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => validationImc()}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
       </View>
       <ResultImc resultImc={imc} messageResultImc={messageImc} />
     </View>
